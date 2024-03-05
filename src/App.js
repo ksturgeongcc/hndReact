b12b5d7aec780dde481587d78ab95691cf84eba2
ksturgeong <ksturgeongcc@gmail.com>
2024-03-05 16:11:44 +0000

    adding repo

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Home from './pages/shared/Home';
+
+function App() {
+  return (
+    <>
+      <Home/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/shared/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './home.css';
+
+const Home = () => {
+    return (
+        <>
+            <h1>hello</h1>
+        </>
+    )
+}
+
+export default Home;
