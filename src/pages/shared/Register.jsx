bdc9cc129f6b92c0258c5bda0c6814378d15aedb
kareb <email@example.com>
2024-04-25 23:31:26 +0100

    localhost http

--- src/pages/shared/Register.jsx
+++ src/pages/shared/Register.jsx
@@ -14,7 +14,7 @@ const Register = () => {
 
   const handleRegister = async () => {
     try {
-      await axios.post('https://localhost/api/register', {
+      await axios.post('http://localhost/api/register', {
         patient_number,
         password,
         email,
