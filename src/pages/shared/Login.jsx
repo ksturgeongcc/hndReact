a7594aa68584308497b359c15688db18b41a57e5
kareb <email@example.com>
2024-04-27 00:17:31 +0100

    j

--- src/pages/shared/Login.jsx
+++ src/pages/shared/Login.jsx
@@ -10,7 +10,7 @@ const Login = () => {
 
   const handleLogin = async () => {
     try {
-      const response = await axios.post(`https://hnd-react.vercel.app`+`/api/login`, {
+      const response = await axios.post(`https://hnd-react.vercel.appp`+`/api/login`, {
         patient_number,
         password,
       });
