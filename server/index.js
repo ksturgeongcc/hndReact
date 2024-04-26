94f19b71f1200839ae9c88a7ae4c5685ca4e66f6
kareb <email@example.com>
2024-04-26 23:34:58 +0100

    ADDING KEY

--- server/index.js
+++ server/index.js
@@ -18,8 +18,8 @@ app.use(express.json());
 app.use(cors());
 
 // MongoDB connection
-const MONGO_URI = process.env.MONGO_URI
-mongoose.connect(MONGO_URI, {
+const MONGODB_URI = process.env.MONGODB_URI
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
